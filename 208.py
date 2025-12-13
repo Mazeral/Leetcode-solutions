a64639e109ffaf253365859926748e3b0290ff66
MohammadOSA <mohamedsiddig5026485@gmail.com>
2025-12-13 08:39:17 +0200

    13dec2025 Trie

---
+++ 208.py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for c in word:
+            if c not in current.children:
+                current.children[c] = TrieNode()
+            current = current.children[c]
+        current.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        for c in word:
+            if c not in current.children:
+                return False
+            current = current.children[c]
+        return current.end_of_word
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.root
+        for c in prefix:
+            if c not in current.children:
+                return False
+            current = current.children[c]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
